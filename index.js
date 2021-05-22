aea5afd4066d74690afb509adc8166e5a30ecde1
Alexandru Matei <alexxmatei@gmail.com>
2021-05-22 14:09:43 +0300

    Added base files necessary to host a nodejs server.

---
+++ index.js
@@ -0,0 +1,16 @@
+import { createServer } from 'http';
+import { readFile } from 'fs'; // file system module
+
+/* create a HTTP server object */
+createServer(function (_req, res) {
+    /* asynchronously reads the content of "index.html" file */
+    readFile("index.html", function (_err, content) {
+        /* write a HTTP header with the correct content type to be displayed */
+        res.writeHead(200, { "Content-Type": "text/html" });
+        /* the HTTP server writes a response to the client */
+        /* in this case it writes the index.html file obtained from readFile */
+        res.write(content);
+        return res.end(); // end the response
+    });
+    /* the server object listens to all interfaces present on this computer */
+}).listen(80, "0.0.0.0");
