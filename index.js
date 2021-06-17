0d7b41e9c06e76f97ad5c72e78ff6d92df4a5d79
Alexandru Matei <alexxmatei@gmail.com>
2021-06-17 23:31:36 +0300

    Modified code to handle default root url.

--- index.js
+++ index.js
@@ -4,17 +4,19 @@ import { generateHtmlContent } from "./htmlContent.js";
 
 /* create a HTTP server object */
 createServer(function (req, res) {
-    /* get user's device name first */
-    getUserDeviceName(req, (userDeviceName) => {
-        /* call generateHtmlContent function with the user device name 
-         * so that it will generate a dynamic page using the user's name */
-        const htmlContent = generateHtmlContent(userDeviceName);
-        /* write a HTTP header with the correct content type to be displayed */
-        res.writeHead(200, { "Content-Type": "text/html" });
-        /* the HTTP server writes a response to the client */
-        /* in this case it writes the dynamically generated HTML content */
-        res.write(htmlContent);
-        return res.end(); // end the response
-        /* the server object listens to all interfaces present on this computer */
-    });
+    if (req.url == "/") {
+        /* get user's device name first */
+        getUserDeviceName(req, (userDeviceName) => {
+            /* call generateHtmlContent function with the user device name 
+             * so that it will generate a dynamic page using the user's name */
+            const htmlContent = generateHtmlContent(userDeviceName);
+            /* write a HTTP header with the correct content type to be displayed */
+            res.writeHead(200, { "Content-Type": "text/html" });
+            /* the HTTP server writes a response to the client */
+            /* in this case it writes the dynamically generated HTML content */
+            res.write(htmlContent);
+            return res.end(); // end the response
+            /* the server object listens to all interfaces present on this computer */
+        });
+    }
 }).listen(80, "0.0.0.0");
