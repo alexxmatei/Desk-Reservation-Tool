bd00498ec4cd16f6a19116ea112711a9f4968060
Alexandru Matei <alexxmatei@gmail.com>
2021-06-19 13:13:44 +0300

    Updated HTML content to take a full header message as an argument.

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ createServer(function (req, res) {
     if (req.url == "/") {
         /* get user's device name first */
         getUserDeviceName(req, (userDeviceName) => {
-            /* call generateHtmlContent function with the user device name 
-             * so that it will generate a dynamic page using the user's name */
-            const htmlContent = generateHtmlContent(userDeviceName);
+            /* generate a header message based on the user's device name */
+            const l_headerMessage_s = "Hello " + userDeviceName;
+            /* call generateHtmlContent function with the generated header message
+             * so that it will generate a dynamic page using that header */
+            const htmlContent = generateHtmlContent(l_headerMessage_s);
             /* write a HTTP header with the correct content type to be displayed */
             res.writeHead(200, { "Content-Type": "text/html" });
             /* the HTTP server writes a response to the client */
