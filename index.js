12e532789e98f0ec17ec870ef4293a13c0117c52
Alexandru Matei <alexxmatei@gmail.com>
2021-06-24 05:55:59 +0300

    Modified if formatting.

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ import { handleRootUrl } from "./urlHandling.js";
 
 /* create a HTTP server object */
 createServer(function (req, res) {
-    if (req.url == "/") {
-        handleRootUrl(req, res);
-    }
+    if (req.url == "/") handleRootUrl(req, res);
 /* the server object listens to all interfaces present on this computer */
 }).listen(80, "0.0.0.0");
