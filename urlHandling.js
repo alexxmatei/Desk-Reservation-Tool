b503a09e51e863b245d5f457f1297e3f0aa8ea79
Alexandru Matei <alexxmatei@gmail.com>
2021-06-26 05:03:08 +0300

    Moved the request handling to urlHandling.js file.

--- urlHandling.js
+++ urlHandling.js
@@ -25,3 +25,25 @@ export function handleRootUrl(req, res) {
         return;
     });
 }
+
+/**
+ * Handles a desk url from "/desk1" to "/desk40" request from a device.
+ * @param {IncomingMessage} req
+ * @param {ServerResponse}  res
+ */
+export function handleDeskUrl(req, res) {
+    /* get the desk number from URL */
+    const deskNr = req.url.toString().substr(5);
+    const headerMessage = "Hello, you clicked on desk number " + deskNr;
+    /* call generateHtmlContent function with the selected desk number 
+    * so that it will generate a dynamic page using the selected desk */
+    const htmlContent = generateHtmlContent(headerMessage);
+    /* write a HTTP header with the correct content type to be displayed */
+    res.writeHead(200, { "Content-Type": "text/html" });
+    /* the HTTP server writes a response to the client */
+    /* in this case it writes the dynamically generated HTML content */
+    res.write(htmlContent);
+    res.end(); // end the response
+
+    return;
+}
