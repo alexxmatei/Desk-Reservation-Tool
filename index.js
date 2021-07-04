6c90dd6f70002ab724e8ea5b0ba917353c80b774
Alexandru Matei <alexxmatei@gmail.com>
2021-07-04 11:05:31 +0300

    Updated indenting.

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ import { handleRootUrl, handleDeskUrl, handleInvalidUrl } from "./urlHandling.js
 
 /* create a HTTP server object */
 createServer(function (req, res) {
-    if (req.url == "/") handleRootUrl(req, res);
-    /* Should only work for desks 1 to 40 */
-    else if (req.url.match("^\/desk(?:[1-9]|[1-3][0-9]|40)$")) handleDeskUrl(req, res);
-    /* Handle any other (invalid) URL */
-    else handleInvalidUrl(req, res);
+  if (req.url == "/") handleRootUrl(req, res);
+  /* Should only work for desks 1 to 40 */
+  else if (req.url.match("^\/desk(?:[1-9]|[1-3][0-9]|40)$")) handleDeskUrl(req, res);
+  /* Handle any other (invalid) URL */
+  else handleInvalidUrl(req, res);
 }).listen(80, "0.0.0.0"); /* the server object listens to all interfaces present on this computer */
