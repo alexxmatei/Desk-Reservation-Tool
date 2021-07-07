fae5368ca7338701069f83876f81732e03c41be3
Alexandru Matei <alexxmatei@gmail.com>
2021-07-07 19:54:23 +0300

    Modified program to only generate cookies when they are missing.

--- index.js
+++ index.js
@@ -4,28 +4,28 @@ import cookie from "cookie";
 
 /* create a HTTP server object */
 createServer(function (req, res) {
-  /* parse the cookies on the request */
-  var cookies = cookie.parse(req.headers.cookie);
+  /* parse the cookies on the request, if available */
+  var cookies = cookie.parse(req.headers.cookie || '');
 
   /* If both test1 and test2 cookies are not set */
-  if ( !( Object.keys(cookies).includes("test 1") && Object.keys(cookies).includes("test 2") ) ) {
+  if (!(Object.keys(cookies).includes("test 1") && Object.keys(cookies).includes("test 2"))) {
     /* Set the cookies test1 and test2 then redirect to root URL */
     /* Response code 302 Found, is used to redirect the user to the URL given in the Location header */
     res.writeHead(302, {
-        'Set-Cookie':[
-            'test 1=1; Max-Age=' + 60, /* cookie will expire in 60 seconds after being set */
-            'test 2=2; expires=' + new Date(new Date().getTime() + 30 * 60000).toUTCString() /* cookie will expire in 30 minutes after being set */
-        ],
-        /* Redirect user to root url */
-        'Location': '/'
+      'Set-Cookie': [
+        'test 1=1; Max-Age=' + 60, /* cookie will expire in 60 seconds after being set */
+        'test 2=2; Expires=' + new Date(new Date().getTime() + 30 * 60000).toUTCString() /* cookie will expire in 30 minutes after being set */
+      ],
+      /* Redirect user to root url */
+      'Location': '/'
     });
-        res.end();
-        return;
+    res.end();
   }
-
+  else {
     if (req.url == "/") handleRootUrl(req, res);
     /* Should only work for desks 1 to 40 */
     else if (req.url.match("^\/desk(?:[1-9]|[1-3][0-9]|40)$")) handleDeskUrl(req, res);
     /* Handle any other (invalid) URL */
     else handleInvalidUrl(req, res);
+  }
 }).listen(80, "0.0.0.0"); /* the server object listens to all interfaces present on this computer */
