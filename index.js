9e994dc842c02ccece07f4359638a42317b7ea58
Alexandru Matei <alexxmatei@gmail.com>
2021-07-15 20:53:24 +0300

    Create a login page where cookies will be created.

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 import { createServer } from "http";
-import { handleRootUrl, handleDeskUrl, handleInvalidUrl } from "./urlHandling.js";
+import { handleRootUrl, handleDeskUrl, handleInvalidUrl, displayLoginPage } from "./urlHandling.js";
 import { setCookiesAndRedirectToRoot as setCookiesAndRedirectToRoot } from "./handleUserData.js";
 import cookie from "cookie";
 
 /* create a HTTP server object */
 createServer(function (req, res) {
+  const baseURL = 'http://' + req.headers.host + '/';
+  const reqUrl = new URL(req.url, baseURL);
+  let nameQuery = reqUrl.searchParams.get("name");
+  let colorQuery = reqUrl.searchParams.get("color");
   /* parse the cookies on the request, if available */
   let cookies = cookie.parse(req.headers.cookie || '');
 
-  /* If both test1 and test2 cookies are not set */
-  const cookiesNotSet_b = new Boolean(!(Object.keys(cookies).includes("test 1") && Object.keys(cookies).includes("test 2")));
+  /* If both color and name cookies are not set */
+  const cookiesNotSet_b = new Boolean(!(Object.keys(cookies).includes("color") && Object.keys(cookies).includes("name")));
+  /* Name must always be send through the query, if it is null, the form was not yet sent */
+  const formNotSent_b = new Boolean(nameQuery == null);
+
 
   if (cookiesNotSet_b == true) {
-    setCookiesAndRedirectToRoot(res);
+    if (formNotSent_b == true) {
+      displayLoginPage(res);
+    }
+    else {
+      setCookiesAndRedirectToRoot(res, nameQuery, colorQuery);
+    }
   }
   else {
     if (req.url == "/") handleRootUrl(req, res);
